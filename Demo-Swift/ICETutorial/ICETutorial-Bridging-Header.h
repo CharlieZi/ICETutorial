6849e421a3baf6176a52a100932aa5c4e001c4c2
Charlie Zi <charlie.yaha@gmail.com>
2015-04-29 20:48:38 +0800

    Swift-Demo and clean up

---
+++ Demo-Swift/ICETutorial/ICETutorial-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ICETutorialPage.h"
+#import "ICETutorialStyle.h"
+#import "ICETutorialController.h"
